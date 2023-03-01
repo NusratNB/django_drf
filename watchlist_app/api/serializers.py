147544caa1ee9ae338299d9d1ec3b71ba3570470
Nusrat Ismoilov <nusrat.bbismoilov@gmail.com>
2023-03-01 20:15:54 +0900

    Update

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -4,13 +4,13 @@ from ..models import WatchList, StreamPlatform
 
 class WatchListSerializer(serializers.ModelSerializer):
 
-
     class Meta:
         model = WatchList
         fields = "__all__"
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
-    
+
+    watchlist = WatchListSerializer(many=True, read_only=True)    
     class Meta:
         model = StreamPlatform
         fields = "__all__"
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -13,6 +13,7 @@ class StreamPlatform(models.Model):
 class WatchList(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
