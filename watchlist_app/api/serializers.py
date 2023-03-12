772ccaa8410106e2bb001c970e2311c5092e7bbb
Nusrat Ismoilov <nusrat.bbismoilov@gmail.com>
2023-03-12 23:52:50 +0900

    Update

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -10,7 +10,8 @@ class WatchListSerializer(serializers.ModelSerializer):
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
 
-    watchlist = WatchListSerializer(many=True, read_only=True)    
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
     class Meta:
         model = StreamPlatform
         fields = "__all__"
