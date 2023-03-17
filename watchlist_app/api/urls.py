a953f055e78ebc2d23f9c36f1ceaf5d147a01f6f
nb2904 <44426608+NusratNB@users.noreply.github.com>
2023-03-17 19:24:11 +0900

    Update

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,8 +3,8 @@ from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatf
 
 urlpatterns = [
     path('watchlist/', WatchListAV.as_view(), name='watch-list'),
-    path('watchlist/<int:pk>/', WatchListDetailAV.as_view(), name='watchlist-detail'),
+    path('watchlist/<int:pk>/', WatchListDetailAV.as_view(),
+         name='watchlist-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
-
 ]
