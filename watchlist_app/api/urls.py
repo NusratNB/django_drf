e587f02165a15ab9938af16a7a2a5ed8e3446071
nb2904 <44426608+NusratNB@users.noreply.github.com>
2023-03-20 18:18:18 +0900

    Update

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,11 +3,16 @@ from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatf
 
 urlpatterns = [
     path('watchlist/', WatchListAV.as_view(), name='watch-list'),
-    path('watchlist/<int:pk>/', WatchListDetailAV.as_view(), name='watchlist-detail'),
+    path('watchlist/<int:pk>/', WatchListDetailAV.as_view(),
+         name='watchlist-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
-    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
-    
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail")
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(),
+         name='streamplatform-detail'),
+
+    # path('review/', ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail")
+
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name="review-detail")
 
 ]
