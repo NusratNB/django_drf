f4c5d26ee5fd6a1c23c8f0923860f89c21da38c7
nb2904 <44426608+NusratNB@users.noreply.github.com>
2023-04-05 17:57:17 +0900

    Update

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
