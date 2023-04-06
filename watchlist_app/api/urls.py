2f91aabed83a57d265b9e8c219ea4f9d4d6a5165
nb2904 <44426608+NusratNB@users.noreply.github.com>
2023-04-06 13:21:28 +0900

    Update

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     # path('review/', ReviewList.as_view(), name='review-list'),
     # path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail")
 
-    path('<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
-    path('<int:pk>/reviews', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name="review-detail")
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('<int:pk>/reviews/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail")
 
 ]
